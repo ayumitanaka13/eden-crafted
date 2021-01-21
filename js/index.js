e32fa4d366400a94247ab8f5aea36b9319eb63e4
ayumitanaka <tanakayumi13@gmail.com>
2021-01-21 10:45:37 -0800

    modified base.scss

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-// window.addEventListener( "scroll", function(){
-
-//     let scroll = window.pageYOffset;
-    
-//     if( scroll > 4000 ){ 
-//       document.body.style.backgroundColor = '#FFC400';
-//     }else if( scroll > 3000 ){
-//       document.body.style.backgroundColor = '#43A047';
-//     }else if( scroll > 2000 ){
-//       document.body.style.backgroundColor = '#FF6F00';
-//     }else if( scroll > 1000 ){
-//       document.body.style.backgroundColor = '#0091EA';
-//     }else{
-//       document.body.style.backgroundColor = '#FF4081';
-//     }
-    
-// });
+$(document).ready(function() {
+    $('.drawer').drawer();
+  });
